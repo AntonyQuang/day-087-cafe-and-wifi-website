2ea591e0de2a9739fd4556a9ca31fa1911ce6bab
Antony Quang <antony.quang@googlemail.com>
2022-08-11 21:05:19 +0100

    Add SQLite functionality

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
-import csv
+from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
+
+
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+db = SQLAlchemy(app)
 Bootstrap(app)
+
+all_cafes = []
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cafe and Wifi Website.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        dictionary = {}
+
+        for column in self.__table__.columns:
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
 
 
 class CafeForm(FlaskForm):
@@ -29,30 +60,28 @@ def home():
 
 @app.route('/add', methods=["GET", "POST"])
 def add_cafe():
-    form = CafeForm()
-    if form.validate_on_submit():
-        new_row = []
-        data = form.data
-        del data["submit"]
-        del data["csrf_token"]
-        for value in data.values():
-            new_row.append(value)
-        with open("cafe-data.csv", encoding='utf-8', newline='', mode="a") as csv_file:
-            writer_object = csv.writer(csv_file)
-            writer_object.writerow(new_row)
-        return redirect(url_for('cafes'))
-
-    return render_template('add.html', form=form)
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return render_template('add.html', form=new_cafe)
 
 
 @app.route('/cafes')
 def cafes():
-    with open('cafe-data.csv', encoding='utf-8', newline='') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
-        list_of_rows = []
-        for row in csv_data:
-            list_of_rows.append(row)
-    return render_template('cafes.html', cafes=list_of_rows)
+    all_cafes = db.session.query(Cafe).all()
+    cafes = [cafe.to_dict() for cafe in all_cafes]
+    return render_template('cafes.html', cafes=cafes)
 
 
 if __name__ == '__main__':
