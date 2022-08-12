f5692caa97547be30e2c3dc24b5476394246fdb2
Antony Quang <antony.quang@googlemail.com>
2022-08-12 22:46:59 +0100

    Add "add" functionality

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired, URL
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cafe and Wifi Website.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 db = SQLAlchemy(app)
 Bootstrap(app)
 
 all_cafes = []
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cafe and Wifi Website.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 # Cafe TABLE Configuration
 class Cafe(db.Model):
@@ -40,15 +39,17 @@ class Cafe(db.Model):
         return dictionary
 
 
-
 class CafeForm(FlaskForm):
-    cafe = StringField('Cafe name', validators=[DataRequired()])
-    location = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
-    open = StringField('Opening Time (E.g. 8AM)', validators=[DataRequired()])
-    close = StringField('Closing Time (E.g. 11:30PM)', validators=[DataRequired()])
-    coffee = SelectField('Coffee Rating', choices=["☕", "☕☕", "☕☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
-    wifi = SelectField('Wifi Rating', choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
-    power = SelectField('Wifi Rating', choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
+    name = StringField('Cafe name', validators=[DataRequired()])
+    map_url = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
+    img_url = StringField('Image of Cafe (URL)', validators=[DataRequired(), URL()])
+    loc = StringField('Location of Cafe', validators=[DataRequired()])
+    seats = StringField('Number of Seats', validators=[DataRequired()])
+    toilets = BooleanField('Has Toilets')
+    wifi = BooleanField('Has Wifi')
+    sockets = BooleanField('Has Sockets')
+    calls = BooleanField('Can Take Calls')
+    coffee_price = StringField('Coffee price', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -60,21 +61,24 @@ def home():
 
 @app.route('/add', methods=["GET", "POST"])
 def add_cafe():
-    new_cafe = Cafe(
-        name=request.form.get("name"),
-        map_url=request.form.get("map_url"),
-        img_url=request.form.get("img_url"),
-        location=request.form.get("loc"),
-        has_sockets=bool(request.form.get("sockets")),
-        has_toilet=bool(request.form.get("toilet")),
-        has_wifi=bool(request.form.get("wifi")),
-        can_take_calls=bool(request.form.get("calls")),
-        seats=request.form.get("seats"),
-        coffee_price=request.form.get("coffee_price"),
-    )
-    db.session.add(new_cafe)
-    db.session.commit()
-    return render_template('add.html', form=new_cafe)
+    form = CafeForm()
+    if form.validate_on_submit():
+        new_cafe = Cafe(
+            name=request.form.get("name"),
+            map_url=request.form.get("map_url"),
+            img_url=request.form.get("img_url"),
+            location=request.form.get("loc"),
+            seats=request.form.get("seats"),
+            has_sockets=bool(request.form.get("sockets")),
+            has_toilet=bool(request.form.get("toilet")),
+            has_wifi=bool(request.form.get("wifi")),
+            can_take_calls=bool(request.form.get("calls")),
+            coffee_price=request.form.get("coffee_price"),
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
+        return redirect(url_for("cafes"))
+    return render_template('add.html', form=form)
 
 
 @app.route('/cafes')
